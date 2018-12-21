21911eb5b83752bdfcd601799050f2e95f0f33c8
3tch-a-sketch <sametchells@icloud.com>
2018-12-21 11:33:32 +0000

    Added params

--- powershell/zip_maker.ps1
+++ powershell/zip_maker.ps1
@@ -1,5 +1,10 @@
 ﻿param(
-[string]$path
-
-
+[string]$path = '',
+[string]$outpath = '',
+[string]$maxsize = '',
+[switch]$debug = $false
 )
+
+
+
+if($maxsize -contains 0 -or 1 -or 2 -or 3 -or 4 -or 5 -or 6 -or 7 -or 8 -or 9){ }
