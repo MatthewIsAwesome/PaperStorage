9e06970851007c61b8e8fe0fd0f6e2d2ebe95fc9
3tch-a-sketch <sametchells@icloud.com>
2018-12-21 12:33:16 +0000

    Added Path creation

--- powershell/zip_maker.ps1
+++ powershell/zip_maker.ps1
@@ -21,6 +21,8 @@ zip_maker.ps1 -path[input path] -outpath[output path] -maxsize[zip size in KB] -
 
 
 if(![System.IO.File]::Exists($outpath)){
-
-
+    mkdir $outpath
+    echo 'Creating Path'
  }
+
+
