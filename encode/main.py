3f59bf8a9166dc9bb0b75f61a43d3a5749dba2a1
MatthewIsAwesome <matthewmiles2@gmail.com>
2018-12-21 10:41:50 +0000

    working on encode main.py

--- encode/main.py
+++ encode/main.py
@@ -1,9 +1,25 @@
 import matplotlib
 import sys
 
-args = sys.argv
+def path(pathName):
+    print("path is {}".format(pathName))
 
+
+args = sys.argv
+print(args)
+currentArg = 0
 for arg in args:
+    print("current arg is {} at position {}".format(arg, currentArg))
     if arg[0] == "-":
+        #Checks for a tack in the first position for tags
+        arg = list(arg)
+        
         if arg[1] == "-":
-            #
+            #Checks for a tack in the second position of string for whole word tags
+
+
+            if arg == "path":
+                path(arg[currentArg+1])
+        if arg == "p":
+            path(arg[currentarg+1])
+    currentArg += 1
