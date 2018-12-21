4af2b0fe5b3227e5fdf0c75056f4a3e2dd2031d8
MatthewIsAwesome <matthewmiles2@gmail.com>
2018-12-21 13:43:21 +0000

    reduced warnings

--- powershell/zip_maker.ps1
+++ powershell/zip_maker.ps1
@@ -1,12 +1,12 @@
 ﻿
 param(
-    
+
     [Parameter(Mandatory=$true)]
     [string]$path,
-    
+
     [Parameter(Mandatory=$true)]
     [string]$outpath,
-    
+
     [Parameter(Mandatory=$true)]
     [int32]$maxsize
 )
@@ -15,13 +15,11 @@ $usage = '
 Usage:
 
 zip_maker.ps1 -path[input path] -outpath[output path] -maxsize[zip size in KB] -debug[True/False]
-
-
 '
-
+Write-Output echo $usage
 
 $test = Test-Path -Path $outpath
 
 if($test -eq 'True'){
-    
+
 }
