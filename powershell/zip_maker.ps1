06db85bc0c50b4d53e2c4929fb9e579d1a035b28
3tch-a-sketch <sametchells@icloud.com>
2018-12-21 15:00:10 +0000

    Added Logging

--- powershell/zip_maker.ps1
+++ powershell/zip_maker.ps1
@@ -8,28 +8,28 @@ param(
     [string]$outpath,
     
     [Parameter(Mandatory=$true)]
-    [int32]$maxsize
+    [int32]$maxsize,
+
+    [switch]$debugger
 )
 
 $usage = '
 Usage:
 
-zip_maker.ps1 -path[input path] -outpath[output path] -maxsize[zip size in Bytes] -debug[True/False]
+zip_maker.ps1 -path[input path] -outpath[output path] -maxsize[zip size in Bytes] -debugger[True/False]
+
 
 
 '
 
-
-$test = Test-Path -Path $outpath
-
-if($test -eq 'True'){ echo 'Found Path' }
-
-if($test -eq 'False'){mkdir $outpath 
-echo 'Making Path' }
-
 cd $path
-$name = ls.name
-$size = ls.length
+
+$ls = ls
+$name = $ls.Name
+$size = $ls.Length
+
+
+
 $join = $name + '' + $size
 if($debug -eq 'true'){echo $join}
-$join | Out-File '\tmp\lstable.txt'
+$join | Out-File '$($PSScriptRoot)\tmp\lstable.txt'
