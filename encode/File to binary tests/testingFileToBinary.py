7dd97c180a13cdd17beae90d1f2697222af5ea7a
MatthewIsAwesome <matthewmiles2@gmail.com>
2019-01-05 21:18:45 +0000

    Successfully duplicated .zip via binary!

--- encode/File to binary tests/testingFileToBinary.py
+++ encode/File to binary tests/testingFileToBinary.py
@@ -1,6 +1,17 @@
-with open("tmp.zip", "r") as f:
-	for line in f:
-		print(line)
+import base64
+
+with open('exampleZip.zip','rb') as imageFile:
+    str = base64.b64encode(imageFile.read())
+
+imageBytes = base64.decodebytes(str)
+imageBinary = "".join(["{:08b}".format(x) for x in imageBytes])
+
+print(imageBinary)
+
+
+with open("duplicatedZip.zip", "wb") as f:
+	f.write(imageBytes)
+
 
 
 
