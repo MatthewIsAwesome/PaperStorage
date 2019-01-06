6d5b66b3ab311e03d2a9822edffe060e24ec9fa2
3tch-a-sketch <sametchells@icloud.com>
2019-01-06 19:02:45 +0000

    Recursive printing

--- powershell/Encode/Print_spool.ps1
+++ powershell/Encode/Print_spool.ps1
@@ -16,14 +16,20 @@ mode 2 start direct printing this can be problematic if on a odd network arrange
 
 
 '
+$outnum = 1
+if($mode -eq 2){
+    $defaultprint = Get-WmiObject -Query " SELECT * FROM Win32_Printer WHERE Default=$true"
+    $printname = $defaultprint.ShareName
+    $printname | Set-PrintConfiguration -DuplexingMode TwoSidedLongEdge
 
+    echo "Found $($printname) as default printer"
+    if($confirm -ne 1){throw 'Exiting to continue set the flag -confirm to 1'}
 
-$defaultprint = Get-WmiObject -Query " SELECT * FROM Win32_Printer WHERE Default=$true"
-$printname = $defaultprint.ShareName
-$printname | Set-PrintConfiguration -DuplexingMode TwoSidedLongEdge
+    Get-ChildItem -Path $imginp | ForEach-Object{
+        $content = Get-Content
+        $content | Out-Printer -Name $outnum
+        $outnum += 1
+    }
 
-echo "Found $($printname) as default printer"
-if($confirm -ne 1){throw 'Exiting to continue set the flag -confirm to 1'}
-
-
- 
+    echo 'process finished'
+}
