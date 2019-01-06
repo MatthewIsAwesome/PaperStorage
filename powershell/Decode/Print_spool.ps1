57798cb30040aeb6fbb86771e6052c6d4bc6aacf
3tch-a-sketch <sametchells@icloud.com>
2019-01-06 18:39:10 +0000

    Added modes and confirm

--- powershell/Decode/Print_spool.ps1
+++ powershell/Decode/Print_spool.ps1
@@ -1,12 +1,25 @@
 ﻿param(
 
+[Parameter(mandatory = $true)]
+[int32]$mode,
 
+[Parameter(mandatory = $false)]
+[int32]$confirm
 
 )
+
+$usage = '
+
+
+
+
+'
+
 
 $defaultprint = Get-WmiObject -Query " SELECT * FROM Win32_Printer WHERE Default=$true"
 $printname = $defaultprint.ShareName
 $printname | Set-PrintConfiguration -DuplexingMode TwoSidedLongEdge
 
 echo "Found $($printname) as default printer"
-echo 
+if($confirm -ne 1){throw 'Exiting to continue set the flag -confirm to 1'}
+ 
