97799e5720297ffc15fe0c4bd65c7614f5485b48
3tch-a-sketch <sametchells@icloud.com>
2019-01-15 17:46:08 +0000

    drop down

--- powershell/GUI test.ps1
+++ powershell/GUI test.ps1
@@ -1,14 +1,10 @@
-﻿<# This form was created using POSHGUI.com  a free online gui designer for PowerShell
-.NAME
-    Paper Storage
-#>
-
-Add-Type -AssemblyName System.Windows.Forms
+﻿Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
 $Window                          = New-Object system.Windows.Forms.Form
-$Window.ClientSize               = '583,592'
+$Window.ClientSize               = '583,544'
 $Window.text                     = "Paper Storage GUI"
+$Window.BackColor                = "#ffffff"
 $Window.TopMost                  = $false
 
 $TextBox1                        = New-Object system.Windows.Forms.TextBox
@@ -19,6 +15,23 @@ $TextBox1.height                 = 44
 $TextBox1.location               = New-Object System.Drawing.Point(14,35)
 $TextBox1.Font                   = 'Microsoft Sans Serif,10'
 
-$Window.controls.AddRange(@($TextBox1))
+$Testtip                         = New-Object system.Windows.Forms.ToolTip
+$Testtip.ToolTipTitle            = "Put the path to the folder you want to paperback"
+$Testtip.isBalloon               = $true
+
+$ComboBoxEncodeDecode            = New-Object system.Windows.Forms.ComboBox
+$ComboBoxEncodeDecode.text       = "Encode"
+$ComboBoxEncodeDecode.DropDownStyle = 'DropDownList'
+$ComboBoxEncodeDecode.Items.Add('Encode')
+$ComboBoxEncodeDecode.Items.Add('Decode')
+$ComboBoxEncodeDecode.width      = 100
+$ComboBoxEncodeDecode.height     = 20
+$ComboBoxEncodeDecode.location   = New-Object System.Drawing.Point(142,171)
+$ComboBoxEncodeDecode.Font       = 'Microsoft Sans Serif,10'
+
+$Window.controls.AddRange(@($TextBox1,$ComboBoxEncodeDecode))
+
+$Window.Add_BackColorChanged({  })
+
 
 $Window.ShowDialog()
